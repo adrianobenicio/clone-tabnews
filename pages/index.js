6ab8d5b9729239ac544558e1947cc2a84c90a93d
adrianobenicio <adriano171@gmail.com>
2024-06-22 18:29:22 +0000

    altera a frase da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Ariane, eu te amo demais!</h1>
+function Home() {
+    return (
+        <body>
+            <h1>Ariane, eu te amo demais!!!</h1>
+            <p>Você é a mulher da minha vida inteira!</p>
+            <p>Feliz dia dos Bês!</p>
+        </body>
+    );
 }
 
 export default Home;
